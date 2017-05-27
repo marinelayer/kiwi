da9500dc383ee8812d94ddc2ae22fca34afaca47
Michael Clark <michaeljclark@mac.com>
2017-05-27 16:56:27 +1200

    Print apple variable in hello_world examples

--- example/hello_world.c
+++ example/hello_world.c
@@ -3,12 +3,15 @@
 
 static const char *text = "";
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp, char **applep)
 {
 	printf("prog=%s\n", argv[0]);
 	printf("argc=%d\n", argc);
 	for (size_t i = 1; i < argc; i++) {
 		printf("argv[%zu]=\"%s\"\n", i, argv[i]);
+	}
+	while (*applep != NULL) {
+		printf("apple: %s\n", *applep++);
 	}
 	void *p = malloc(128);
 	printf("text  : %p\n", text);
