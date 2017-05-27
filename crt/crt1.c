9c99efd50f6def82a0dd9f45b5413eb5bc723fcf
Michael Clark <michaeljclark@mac.com>
2017-05-27 16:54:49 +1200

    Modify forward declarations in crt1.c

--- crt/crt1.c
+++ crt/crt1.c
@@ -5,14 +5,14 @@
 
 int main();
 _Noreturn int __libc_start_main(int (*)(), int, char **);
-void __rebase_macho(void* mh, intptr_t slide);
+void __rebase_macho(uintptr_t, intptr_t);
 
 void _start_c(long *p, uintptr_t image_base, uintptr_t slide)
 {
 	int argc = p[0];
 	char **argv = (void *)(p+1);
 	if (slide > 0) {
-		__rebase_macho((void*)image_base, slide);
+		__rebase_macho(image_base, slide);
 	}
 	__libc_start_main(main, argc, argv);
 }
