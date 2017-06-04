5463cef145a49638f973816814dce3a4727bc7ac
Michael Clark <michaeljclark@mac.com>
2017-06-05 11:33:49 +1200

    Add #ifdef __cplusplus to mach_time.h

--- include/mach/mach_time.h
+++ include/mach/mach_time.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct mach_timebase_info {
 	uint32_t numer;
 	uint32_t denom;
@@ -12,5 +16,9 @@ typedef struct mach_timebase_info mach_timebase_info_data_t;
 
 uint64_t mach_absolute_time(void);
 int mach_timebase_info(struct mach_timebase_info *info);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
